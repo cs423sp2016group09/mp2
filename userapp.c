5894bf924b9315f9d5d7ae66d3a634050a0a4a8f
Vassil Mladenov <vmladenov@icloud.com>
2016-03-07 15:06:29 -0600

    added framework for userapp

--- userapp.c
+++ userapp.c
@@ -1,7 +1,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "userapp.h"
 
 long long unsigned fac(long long n) {
@@ -10,27 +10,36 @@ long long unsigned fac(long long n) {
 	return n*fac(n-1);
 }
 
+void REGISTER() {
+    
+}
+
+void DEREGISTER() {
+    
+}
+
+void YIELD() {
+
+}
+static int pid;
+static FILE * statusfile;
 int main(int argc, char* argv[]) {
-	  // get pid 
-	  int pid = getpid();
-
-	  // write pid as a string to the proc filesystem
-	  FILE * statusfile = fopen("/proc/mp1/status", "r+");
-
-	  if (statusfile == NULL ) return 1;
-	  fprintf(statusfile, "%d", pid);
-	  // read the proc fs
-
-	  fclose(statusfile);
-	
-	  long long x = 0;
-
-	  int i, j;
-	  for (j=0; j < 10000; j++) {
-		  for (i = 0; i < 10000; i++) {
-			  x = fac(33);		
-	  	  }
-	  }
-	
-          return 0;
+    // get pid 
+    pid = getpid();
+    // write pid as a string to the proc filesystem
+    statusfile = fopen("/proc/mp2/status", "r+");
+    if (statusfile == NULL) exit(1);
+    fprintf(statusfile, "%d", pid);            	  
+    // read the proc fs            
+    fclose(statusfile);
+    long long x = 0;
+                
+    int i, j;
+    for (j=0; j < 10000; j++) {
+        for (i = 0; i < 10000; i++) {
+            x = fac(33);		
+        }
+    }
+                	
+    return 0;
 }
