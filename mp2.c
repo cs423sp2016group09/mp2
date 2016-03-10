798a9257572a0615e1bde630bc434a4ab32126fd
Laurynas <tamulev2@illinois.edu>
2016-03-09 23:53:05 -0600

    Commented

--- mp2.c
+++ mp2.c
@@ -11,7 +11,7 @@
 #include <linux/cache.h>
 #include <linux/kthread.h>
 #include <linux/slab.h>
-// #include <linux/mutex.h>
+#include <linux/mutex.h>
 #include "mp2_given.h"
 
 MODULE_LICENSE("GPL");
@@ -41,11 +41,8 @@ typedef struct mp2_task_struct {
     struct task_struct *task;
     struct list_head task_node;
     struct timer_list task_timer;
-
     unsigned long deadline;
-
     unsigned int state;
-
     unsigned int pid;
     unsigned long cputime;
     unsigned long period;
@@ -58,6 +55,7 @@ static mp2_struct* currently_running_task;
 static LIST_HEAD(head_task);
 static int finished_writing;
 
+// find the task with highest priority
 static mp2_struct *find_shortest_period(void) {
     mp2_struct *i;
 
@@ -84,20 +82,19 @@ static mp2_struct *find_shortest_period(void) {
 }
 
 static int dispatch_thread_function(void *arg){
+    struct sched_param sparam;
+
+    mp2_struct *next_task;
     while(1) { // woken up
         printk(KERN_ALERT "WOKEN UP\n");
         
-        mp2_struct *next_task;
-        
-        struct sched_param sparam;
-
         
         printk(KERN_ALERT "finding shortest\n");
 
 
         next_task = find_shortest_period();
 
-
+        //checks if no next task
         if (next_task == NULL) { // no shortest period
             printk(KERN_ALERT "DID NOT FIND ONE WITH THE SHORTEST PERIOD");
             // no new task in the ready state,   
@@ -109,6 +106,7 @@ static int dispatch_thread_function(void *arg){
             // not sure if we should make it ready or running, since it's not context switching
             // currently_running_task->state = RUNNING;
         } else {
+        // if the next task exists
             printk(KERN_ALERT "Will be switching to PID: %u", next_task->pid);
             // there was a currently running task and task has not ended
             if (currently_running_task != NULL && currently_running_task->task != NULL) {
@@ -139,7 +137,7 @@ static int dispatch_thread_function(void *arg){
 
 static void wake_up_timer_function(unsigned long arg){
     unsigned long flags;
-    
+    //parsing back the task from long to mp2_struct
     mp2_struct *waking_task = (mp2_struct *)arg;
     printk(KERN_ALERT "Timer bell rung! \t I belong to %u!\n", waking_task->pid);
     spin_lock_irqsave(&my_lock, flags);
@@ -147,6 +145,7 @@ static void wake_up_timer_function(unsigned long arg){
     spin_unlock_irqrestore(&my_lock, flags);
 
     printk(KERN_ALERT "Waking up dispatch\n");
+    //wakes up the dispatching thread
     wake_up_process(dispatch_thread.task);
 }
 
@@ -156,17 +155,18 @@ static int admission_control(unsigned long new_task_period, unsigned long new_ta
     unsigned long task_ratio;
     unsigned long new_task_ratio;
     mp2_struct *i;
-
+  
     new_task_ratio = 1000 * new_task_cputime / new_task_period;
 
     existing_ratio_sum = 0;
     mutex_lock(&mutex_list);
+    //calculates the sum of ratios
     list_for_each_entry(i, &head_task, task_node) {
         task_ratio = 1000 * i->cputime / i->period;
         existing_ratio_sum += task_ratio;
     }
     mutex_unlock(&mutex_list);
-
+    //checks if adding new task ratio do not exceed the limit
     return existing_ratio_sum + new_task_ratio <= 693;
 }
 
@@ -176,7 +176,7 @@ static void REGISTER(unsigned int pid, unsigned long period, unsigned long cputi
 
     printk(KERN_ALERT "Received REGISTER message: %u\n", pid);
 
-    // admission control
+    // admission control check
     passes_admission_control = admission_control(period, cputime);
 
     if (!passes_admission_control) {
@@ -192,18 +192,14 @@ static void REGISTER(unsigned int pid, unsigned long period, unsigned long cputi
     new_task->pid = pid;
     new_task->cputime = cputime;
     new_task->period = period; 
-
     new_task->state = SLEEPING;
-    
 
     new_task->deadline = jiffies + msecs_to_jiffies(new_task->period);
-
-    /* setup your timer to call wakeupfun */
+    // setup your timer to call wake_up_timer_function
     setup_timer(&(new_task->task_timer), wake_up_timer_function, (unsigned long) new_task);
 
     new_task->task = find_task_by_pid(new_task->pid);
 
-    // printk(KERN_ALERT "successfully set task structure to sleeping\n");
     list_add(&(new_task->task_node), &head_task);
 
 }
@@ -214,13 +210,13 @@ static void DEREGISTRATION(unsigned int pid){
     printk(KERN_ALERT "DEREGISTRATION for pid: %u\n", pid);
 
     mutex_lock(&mutex_list);
+    //finds the task to deregister by its pid and deregisters
     list_for_each_entry_safe(cursor, next, &head_task, task_node) {
         if (cursor->pid == pid){
             del_timer(&(cursor->task_timer));
             list_del(&(cursor->task_node));
             kmem_cache_free(cache,cursor);
             printk(KERN_ALERT "FOUND PID and deleted!!\n");
-            // break; // stop after removing first pid found
         }    
     }   
     mutex_unlock(&mutex_list);
@@ -228,28 +224,22 @@ static void DEREGISTRATION(unsigned int pid){
 
 static void YIELD(unsigned int pid){
     mp2_struct *i;
-    mp2_struct *next;
-    unsigned long now;
+    mp2_struct *yielding_task;
+
     struct sched_param sparam;
 
     printk(KERN_ALERT "YIELDING for pid: %u\n", pid);
 
-    mp2_struct *yielding_task = NULL;
+    yielding_task = NULL;
     mutex_lock(&mutex_list);
     list_for_each_entry(i, &head_task, task_node) {
         if (i->pid == pid) {
             printk(KERN_ALERT "Found my PID!!\n");
-            
             yielding_task = i;
-            // printk(KERN_ALERT "FOUND PID!!\n");
-            // spin_lock_irqsave(&mp2_spin_lock, 0);
-            // spin_unlock_irqrestore(&mp2_spin_lock, 0);
-            // set_task_state(i->task, TASK_UNINTERRUPTIBLE);
-            // break;
-            
         }    
     }
     mutex_unlock(&mutex_list);
+
     if (yielding_task != NULL && yielding_task->task != NULL) {
         if (jiffies < yielding_task->deadline) {
             printk(KERN_ALERT "Before the deadline, should sleep\n");
@@ -282,26 +272,20 @@ static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, l
     int line_length;
     char * buf_curr_pos;
     mp2_struct *i;
-    
-    // printk(KERN_ALERT "mp2_read called with %zu bytes\n", count);
-
     // must return 0 to signal that we're done writing to cat
     if (finished_writing == 1) {
         finished_writing = 0;
-        // printk(KERN_ALERT "mp2_read finished\n");
         return 0;
     } 
-
     copied = 0;
 
     // allocate a buffer big enough to hold the contents
     buf = (char *) kmalloc(READ_BUFFER_SIZE, GFP_KERNEL);
     memset(buf, 0, READ_BUFFER_SIZE);
     buf_curr_pos = buf;
-    // mutex_lock(&mutex_list);
+
     list_for_each_entry(i, &head_task, task_node) {
 
-        // printk(KERN_ALERT "on entry pid: %u \n", i->pid);
         // allocate line long enoguh to hold the string
         line = kmalloc(LINE_LENGTH, GFP_KERNEL);
         memset(line, 0, LINE_LENGTH);
@@ -314,7 +298,6 @@ static ssize_t mp2_read (struct file *file, char __user *buffer, size_t count, l
 
         kfree(line);
     }
-    // mutex_unlock(&mutex_list);
 
     copy_to_user(buffer, buf, READ_BUFFER_SIZE);
     finished_writing = 1; // return 0 on the next run
@@ -330,12 +313,11 @@ static ssize_t mp2_write (struct file *file, const char __user *buffer, size_t c
     unsigned long period;
     unsigned long cputime;
     
-    // printk(KERN_ALERT "mp2_write called with %zu bytes\n", count);
     // manually null terminate
     buf = (char *) kmalloc(count+1,GFP_KERNEL); 
     copied = copy_from_user(buf, buffer, count);
     buf[count]=0;
-
+	//parses the string and checks
     if (count > 0) {
         char cmd = buf[0];
         switch (cmd){
@@ -369,7 +351,6 @@ int __init mp2_init(void)
 {
 
     cache = kmem_cache_create("cache_name", sizeof(mp2_struct), 0, 0, NULL);
-    /** does not compile, context switch undefined, TODO fix */
 
     #ifdef DEBUG
         printk(KERN_ALERT "MP2 MODULE LOADING\n");
